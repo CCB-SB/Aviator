ab5e8e621a08c952189848664af38e293b469789
Tobias Fehlmann <tobias.fehlmann@ccb.uni-saarland.de>
2020-10-23 06:44:39 +0200

    Update statistics page

--- webserver/main/views.py
+++ webserver/main/views.py
@@ -5,10 +5,9 @@ from django.core import serializers
 from django.core.paginator import Paginator
 from datetime import timedelta, date, datetime
 import json
-from django.contrib.postgres.aggregates import ArrayAgg
-from django.contrib.postgres.aggregates import BoolOr
-from django.db.models.functions import TruncDate
-from django.db.models.functions import Cast
+from django.contrib.postgres.aggregates import ArrayAgg, BoolOr
+from django.db.models.functions import TruncDate, Cast
+from django.db.models import Count
 from django.db import models
 
 # Create your views here.
@@ -63,30 +62,50 @@ def paperData(request):
     data_papers = list(Publication.objects.all().values('pk', 'title', 'url', 'authors', 'abstract', 'year', 'journal', 'pubmed_id', 'contact_mail', 'user_kwds').annotate(websites=ArrayAgg('websites')))
     return JsonResponse({"data": data_papers})
 
+
 def statistics(request):
     context = {}
     context['website_count'] = Website.objects.count()
     context['paper_count'] = Publication.objects.count()
     online = Website.objects.filter(status=True)
     context['online_count'] = online.count()
-    context['offline_count'] = Website.objects.count() - online.count()
+    latest_time = WebsiteCall.objects.latest("datetime")
+    context['temp_offline_count'] = WebsiteCall.objects.filter(
+        datetime__gt=latest_time.datetime - timedelta(days=30)).values("website").annotate(
+        final_ok=BoolOr("ok")).filter(final_ok=True, website__status=False).count()
+    context['offline_count'] = Website.objects.count() - context['online_count'] - context['temp_offline_count']
 
-    current_date = date.today() - timedelta(days=14)
+    current_date = latest_time.datetime - timedelta(days=14)
     stat_names = ""
     stat_online = ""
     stat_offline = ""
+    stat_tmp_offline = ""
     for i in range(0, 14):
         current_date = current_date + timedelta(days=1)
         if i > 0:
             stat_online += ", "
             stat_offline += ", "
+            stat_tmp_offline += ", "
             stat_names += ", "
         stat_names += '"' + str(current_date.day) + "." + str(current_date.month) + "." + str(current_date.year) + '"'
-        ws = WebsiteCall.objects.filter(datetime__date=current_date, ok=True, error="", code=200)
-        stat_online += '"'+str(ws.count())+'"'
-        ws = WebsiteCall.objects.filter(datetime__date=current_date).exclude(ok=True, error="", code=200)
-        stat_offline += '"'+str(+ws.count())+'"'
+        ws_online = WebsiteCall.objects.filter(datetime__date=current_date).values("website").annotate(final_ok=BoolOr("ok")).filter(final_ok=True).count()
+        stat_online += f'"{ws_online}"'
+        ws_tmp_offline = WebsiteCall.objects.filter(
+            datetime__range=(current_date - timedelta(days=30), current_date)).values("website").annotate(
+            final_ok=BoolOr("ok")).filter(final_ok=True, website__status=False).count()
+        stat_tmp_offline += f'"{ws_tmp_offline}"'
+        ws_offline = WebsiteCall.objects.filter(
+            datetime__range=(current_date - timedelta(days=30), current_date)).values("website").annotate(
+            final_ok=BoolOr("ok")).filter(final_ok=False, website__status=False).count()
+        stat_offline += f'"{ws_offline}"'
     context['stat1_names'] = stat_names
     context['stat1_online'] = stat_online
     context['stat1_offline'] = stat_offline
+    context['stat1_tmp_offline'] = stat_tmp_offline
+
+    context["top10_journals"] = Publication.objects.values("journal").annotate(count=Count("journal")).order_by("-count")[:10]
+    journals = [e["journal"] for e in context["top10_journals"]]
+
+
+
     return render(request, 'statistics.html', context)
