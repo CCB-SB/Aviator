b7a68100d1c867d90e0fc4b26b87d6f0d8331eed
Pascal <s9pahirs@uni-saarland.de>
2021-04-23 15:23:24 +0200

    Renamed Pages
    Former-commit-id: 251f1b7bec731a6071e22e0b99ae5284fb30aace

--- webserver/main/urls.py
+++ webserver/main/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path("table_data", view=views.Table.as_view(), name='table_data'),
     path("curated_data", view=views.CuratedTable.as_view(), name='curated_data'),
     path('autocomplete', views.autocomplete, name='autocomplete'),
-    path('enable', views.api, name='api'),
-    path('enabled', views.curated, name='curated'),
+    path('api-instruction', views.api, name='api'),
+    path('api-queried', views.curated, name='curated'),
     path('curated_autocomplete', views.curated_autocomplete, name='curated_autocomplete'),
     path('aviator_api', views.aviator_api, name='aviator_api'),
 ]
