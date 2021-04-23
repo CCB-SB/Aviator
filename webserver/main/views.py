cd767b5eeaf56c6947b72b99a5ca51b72b6f8eb9
Pascal <s9pahirs@uni-saarland.de>
2021-04-23 17:55:54 +0200

    Adjusted numbers

--- webserver/main/views.py
+++ webserver/main/views.py
@@ -110,7 +110,7 @@ def index(request):
     context['overall_calls'] = 0
     context['overall_size'] = 0
     for gs in GlobalStatistics.objects.all():
-        context['overall_calls'] = "{:,}".format(gs.num_calls)
+        context['overall_calls'] = "{:,}".format(gs.num_calls // 1000000000)
         context['overall_size'] = "{:,}".format(gs.data_size)
     return render(request, 'index.html', context)
 
