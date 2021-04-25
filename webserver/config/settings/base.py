3a19d07c3d55bf297dbff08c43b97e4d166fbf6a
Pascal Hirsch <s9pahirs@stud.uni-saarland.de>
2021-04-26 01:34:20 +0200

    inline test
    Former-commit-id: dde87bc2722c15e211ee4ad9eee6a102a5484a5c

--- webserver/config/settings/base.py
+++ webserver/config/settings/base.py
@@ -92,6 +92,7 @@ MIDDLEWARE = [
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(ROOT_DIR('aviator/static'))#str(ROOT_DIR('staticfiles'))
+COMPRESS_ROOT = str(ROOT_DIR('aviator/static'))#str(ROOT_DIR('staticfiles'))
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
