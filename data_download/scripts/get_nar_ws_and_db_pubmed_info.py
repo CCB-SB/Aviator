7bb2bd7a086aa3d678aa1a243cedf316f5859329
Tobias Fehlmann <tobias.fehlmann@ccb.uni-saarland.de>
2021-07-21 22:15:21 +0200

    Add NAR database issue publications starting from 2010

---
+++ data_download/scripts/get_nar_ws_and_db_pubmed_info.py
@@ -0,0 +1,69 @@
+from tqdm import tqdm
+import time
+import pandas as pd
+
+from Bio import Entrez
+
+query = """("Nucleic acids research"[Journal]) AND ((D1[Issue]) OR (Database issue[Issue]) OR W1[Issue] OR (Web Server issue[Issue])) AND (("2010/01/01"[Date - Publication] : "3000"[Date - Publication]))"""
+
+Entrez.email = "tobias.fehlmann@gmail.com"
+Entrez.api_key = "2a26cfc6c7e92ad9eee8a2b29615e8a14209"
+handle = Entrez.esearch(db='pubmed',
+                        retmax='1000000',
+                        retmode='xml',
+                        term=query
+)
+pmids = Entrez.read(handle)
+
+pmid_list = pmids["IdList"]
+
+def fetch_details(id_list):
+    ids = ','.join(id_list)
+    handle = Entrez.efetch(db='pubmed',
+                           retmode='xml',
+                           id=ids)
+    results = Entrez.read(handle)
+    return results
+
+step_size = 1000
+
+def get_author_name(author):
+    if "LastName" in author and "Initials" in author:
+        return f"{author['LastName']} {author['Initials']}"
+    if "LastName" in author:
+        return author['LastName']
+    if "CollectiveName" in author:
+        return author["CollectiveName"]
+    raise RuntimeError("Could not determine author name {}".format(author))
+
+pmid_info = dict()
+for i in tqdm(range(0, len(pmid_list), step_size)):
+    result = fetch_details(pmid_list[i:min(i+step_size, len(pmid_list))])
+    for e in result["PubmedArticle"]:
+        pmid = str(e["MedlineCitation"]["PMID"])
+        article = e["MedlineCitation"]["Article"]
+        abstract = article.get("Abstract", {"AbstractText": [""]})["AbstractText"]
+        abstract_text = "\n".join(str(e) for e in abstract)
+        title = article["ArticleTitle"]
+        # skip editorials
+        if "editorial" in title.lower():
+            continue
+        pubdate = article["Journal"]["JournalIssue"]["PubDate"]
+        year = pubdate.get("Year", pubdate.get("MedlineDate", "")).split(' ')[0]
+        journal_info = article["Journal"]
+        if "AuthorList" in article:
+            authors = ", ".join(get_author_name(a) for a in article["AuthorList"])
+        else:
+            authors = ""
+        pmid_info[pmid] = {
+            "abstract": abstract_text,
+            "title": title,
+            "year": year,
+            "journal": journal_info.get("ISOAbbreviation", journal_info["Title"]),
+            "authors": authors
+        }
+
+
+pmid_tbl = pd.DataFrame.from_dict(pmid_info, orient='index')
+pmid_tbl.index.name = "PMID"
+pmid_tbl.to_csv(snakemake.output.csv, sep='\t')
