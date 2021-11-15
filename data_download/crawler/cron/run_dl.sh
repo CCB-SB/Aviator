46103071bd6f49fe3c2c47a18456e63ba99ce88f
Tobias Fehlmann <tobias.fehlmann@ccb.uni-saarland.de>
2021-11-15 15:27:11 +0000

    Remove stuck chromedriver processes, if any are present

--- data_download/crawler/cron/run_dl.sh
+++ data_download/crawler/cron/run_dl.sh
@@ -12,4 +12,5 @@ rsync -a /local/downloads/aviator/downloads/${TIMESTAMP} ccb-compute2.cs.uni-saa
 python3 trigger_db_update.py --token cRbtB795TX-Xa6Q8_yXf
 ssh ccb-compute2.cs.uni-saarland.de rm -rf /data/aviator_telemetry/${TIMESTAMP}
 rsync -a /local/downloads/aviator/downloads/${TIMESTAMP} main:/share/data/Aviator/crawler_data/ && rm -rf /local/downloads/aviator/downloads/${TIMESTAMP}
+killall chromedriver
 
